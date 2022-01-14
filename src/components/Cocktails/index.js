5f5d61ae09a843f40b7d83826ccdf5a02b9417ee
Graham <graycanny@hotmail.com>
2022-01-14 14:58:23 +0000

    v2

--- src/components/Cocktails/index.js
+++ src/components/Cocktails/index.js
@@ -2,39 +2,48 @@ import React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Cocktail from "../Cocktail";
+import "./Cocktails.css";
 import { useState, useEffect } from "react";
 
-function Cocktails({ cockSearch }) {
+function Cocktails() {
   const [cocktail, setCocktail] = useState("");
-  async function fetchCock() {
-    try {
-      const response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/random.php`
-      );
-      const data = await response.json();
-      console.log(data);
-      setCocktail(data);
-    } catch (err) {
-      console.log("ERROR HERE");
-    }
-  }
+  const [btnClick, setBtnClick] = useState(false);
 
   useEffect(() => {
+    async function fetchCock() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+        );
+        const data = await response.json();
+        console.log(data);
+        setCocktail(data);
+      } catch (err) {
+        console.log("ERROR HERE");
+      }
+    }
     fetchCock();
-    console.log("Data is here", cocktail);
-  }, [cocktail]);
+  }, [btnClick]);
 
   function handleClick() {
-    console.log("HANDLE CLICK FUNCTION");
+    setBtnClick(true);
   }
   console.log(cocktail);
 
   // <h1>Cocktail Name: {cocktail.drinks[0].strDrink}</h1>;
   return cocktail ? (
     <div>
-      <h1>Cocktail Friend</h1>
-      <Button onClick={handleClick}>Search Cocktails</Button>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <nav>
+        <h1>Cocktail Friend</h1>
+      </nav>
+      <Button variant="contained" onClick={handleClick}>
+        Search Cocktails
+      </Button>
+      <TextField
+        id="outlined-basic"
+        label="Search for a cocktail..."
+        variant="outlined"
+      />
       <Cocktail cocktail={cocktail} />
     </div>
   ) : (
