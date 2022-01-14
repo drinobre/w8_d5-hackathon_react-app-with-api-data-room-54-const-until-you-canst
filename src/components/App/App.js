c4c72719d8ba07f52883a7ec859f86a062c27864
AsmaDage <asmadage@gmail.com>
2022-01-14 12:38:16 +0000

    hi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,37 +4,25 @@ import { useState, useRef, useEffect } from "react";
 
 // const url = `www.thecocktaildb.com/api/json/v1/1/`;
 // const url = `www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
-const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+// const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 function App() {
   const textInput = useRef("");
-  const [cockSearch, setCockSearch] = useState("margarita");
-  const [apiCock, setApiCock] = useState("");
+  const [cockSearch, setCockSearch] = useState();
 
-  async function fetchCock() {
-    const response = await fetch(url);
-    const data = await response.json();
-    setApiCock(data);
-  }
-  console.log(apiCock);
+  // function handleChange(event) {
+  //   textInput.current = event.target.value;
+  // }
 
-  useEffect(() => {
-    fetchCock();
-  }, []);
-  function handleChange(event) {
-    textInput.current = event.target.value;
-  }
-  console.log(apiCock);
-
-  function handleClick() {
-    setCockSearch(textInput.current);
-    console.log(cockSearch);
-  }
+  // function handleClick() {
+  //   setCockSearch(textInput.current);
+  //   console.log(cockSearch);
+  // }
 
   return (
     <div className="App">
-      <input type="text" onChange={handleChange}></input>
-      <button onClick={handleClick}>Search</button>
-      <Cocktails apiCock={apiCock} />
+      {/* <input type="text" onChange={handleChange}></input> */}
+      {/* <button onClick={handleClick}>Search</button> */}
+      <Cocktails cockSearch={cockSearch} />
     </div>
   );
 }
--- src/components/Cocktails/index.js
+++ src/components/Cocktails/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Cocktail from "../Cocktail";
+import { useState, useEffect } from "react";
 
-function Cocktails({ apiCock }) {
-  return (
+function Cocktails({ cockSearch }) {
+  const [cocktail, setCocktail] = useState("");
+  async function fetchCock() {
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+      );
+      const data = await response.json();
+      console.log(data);
+      setCocktail(data);
+    } catch (err) {
+      console.log("ERROR HERE");
+    }
+  }
+
+  useEffect(() => {
+    fetchCock();
+    console.log("Data is here", cocktail);
+  }, [handleClick]);
+
+  function handleClick() {
+    console.log("HANDLE CLICK FUNCTION");
+  }
+
+  return setCocktail ? (
     <div>
-      <h1>Cocktail Name: {apiCock.drinks[0].strDrink}</h1>
-      <Cocktail></Cocktail>
+      <button onClick={handleClick}></button>
+      <h1>Cocktail Name: {cocktail.drinks[0].strDrink}</h1>
+      <Cocktail cocktail={...cocktail} />
     </div>
+  ) : (
+    <>Nothing here yet</>
   );
 }
 
