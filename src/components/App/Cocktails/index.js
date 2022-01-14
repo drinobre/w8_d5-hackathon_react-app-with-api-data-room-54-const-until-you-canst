1d8fd3845dc577f7db4c307b315cd8243e703ccf
AsmaDage <asmadage@gmail.com>
2022-01-14 11:40:32 +0000

    .

---
+++ src/components/App/Cocktails/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cocktail from "../Cocktail";
+
+function Cocktails() {
+  return (
+    <div>
+      <Cocktail></Cocktail>
+    </div>
+  );
+}
+
+export default Cocktails;
